f3cc1bfd27c8e4be4bc9ca9fa1c6f56012f467e6
Blake Allen <b.allen82@gmail.com>
2017-03-05 17:24:50 -0600

    Working the way I want, ready for addition of NCDC data

--- server.R
+++ server.R
@@ -1,73 +1,82 @@
-
-
-
 library(leaflet)
-library(RColorBrewer)
 library(maps)
+library(rgdal)
+library(RColorBrewer)
 
-shinyServer(function(input, output, session) {
+server <- (function(input, output, session) {
   values <- reactiveValues(highlight = c())
+  states <- readOGR("shp/cb_2015_us_state_20m.shp",
+                    layer = "cb_2015_us_state_20m", GDAL1_integer64_policy = TRUE)
+  #counties <- readOGR("shp/cb_2015_us_county_20m.shp",
+  #                  layer = "cb_2015_us_county_20m", GDAL1_integer64_policy = TRUE)
   
-  map <- createLeafletMap(session, "map")
   
-  # Draw the given states, with or without highlighting
-  drawStates <- function(stateNames, highlight = FALSE) {
-    states <- map("state", stateNames, plot=FALSE, fill=TRUE)
-    map$addPolygon(I(states$y), I(states$x), I(states$names),
-                   I(lapply(states$names, function(x) {
-                     x <- strsplit(x, ":")[[1]][1]
-                     list(fillColor = colors[[x]])
-                   })),
-                   I(list(fill=TRUE, fillOpacity=0.7, 
-                          stroke=TRUE, opacity=1, color="white", weight=ifelse(highlight, 4, 1)
-                   ))
-    )
-  }
+  ramp <- colorRampPalette(brewer.pal(8,"YlOrRd"))(8)
+  pal <- colorBin(ramp, states$ALAND, bins = 8, pretty = TRUE, na.color = "#808080",
+                  alpha = FALSE, reverse = FALSE)
   
-  # session$onFlushed is necessary to delay the drawing of the polygons until
-  # after the map is created
-  session$onFlushed(once=TRUE, function() {
-    # Get shapes from the maps package
-    drawStates(names(density))
-  })
   
-  # input$map_shape_mouseover gets updated a lot, even if the id doesn't change.
-  # We don't want to update the polygons and stateInfo except when the id
-  # changes, so use values$highlight to insulate the downstream reactives (as 
-  # writing to values$highlight doesn't trigger reactivity unless the new value 
-  # is different than the previous value).
-  observe({
-    values$highlight <- input$map_shape_mouseover$id
-  })
+  # map <- leaflet(states) %>% setView(-94.85, 38, zoom = 4) %>% 
+  #                      setMaxBounds(-180, 17, 180 ,59) %>% 
+  #                      addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+  #                      opacity = 1.0, fillOpacity = 1.0, fillColor = ~pal(states$ALAND),
+  #                      highlightOptions = highlightOptions(color = "white", weight = 2,
+  #                      bringToFront = TRUE)) %>%
+  #                      addLegend("bottomright", pal = pal, values = states$ALAND,
+  #                      title = "State Land Area (km^2)", 
+  #                      labFormat = labelFormat(transform = function(x) 0.000001*x),
+  #                      opacity = 1)
+
+  #no highlighting for now, will add highlighting and dynamic data floater later
+  map <- leaflet(states) %>% addTiles() %>% setView(-94.85, 38, zoom = 4) %>%
+                       setMaxBounds(-180, 17, 180 ,59) %>%
+                       addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+                       opacity = 1.0, fillOpacity = 0.5, fillColor = ~pal(states$ALAND)) %>%
+                       addLegend("bottomright", pal = pal, values = states$ALAND,
+                       title = "State Land Area (km^2)",
+                       labFormat = labelFormat(transform = function(x) 0.000001*x),
+                       opacity = 0.5)
   
-  # Dynamically render the box in the upper-right
-  output$stateInfo <- renderUI({
-    if (is.null(values$highlight)) {
-      return(tags$div("Hover over a state"))
-    } else {
-      # Get a properly formatted state name
-      stateName <- names(density)[getStateName(values$highlight) == tolower(names(density))]
-      return(tags$div(
-        tags$strong(stateName),
-        tags$div(density[stateName], HTML("people/m<sup>2</sup>"))
-      ))
-    }
-  })
+  output$myMap = renderLeaflet(map)
   
-  lastHighlighted <- c()
-  # When values$highlight changes, unhighlight the old state (if any) and
-  # highlight the new state
-  observe({
-    if (length(lastHighlighted) > 0)
-      drawStates(getStateName(lastHighlighted), FALSE)
-    lastHighlighted <<- values$highlight
-    
-    if (is.null(values$highlight))
-      return()
-    
-    isolate({
-      drawStates(getStateName(values$highlight), TRUE)
-    })
-  })
+
+  # # input$map_shape_mouseover gets updated a lot, even if the id doesn't change.
+  # # We don't want to update the polygons and stateInfo except when the id
+  # # changes, so use values$highlight to insulate the downstream reactives (as
+  # # writing to values$highlight doesn't trigger reactivity unless the new value
+  # # is different than the previous value).
+  # observe({
+  #   values$highlight <- input$map_shape_mouseover$id
+  # })
+  # 
+  # # Dynamically render the box in the upper-right
+  # output$stateInfo <- renderUI({
+  #   if (is.null(values$highlight)) {
+  #     return(tags$div("Hover over a state"))
+  #   } else {
+  #     # Get a properly formatted state name
+  #     stateName <- names(density)[getStateName(values$highlight) == tolower(names(density))]
+  #     return(tags$div(
+  #       tags$strong(stateName),
+  #       tags$div(density[stateName], HTML("people/mi<sup>2</sup>"))
+  #     ))
+  #   }
+  # })
+  # 
+  # lastHighlighted <- c()
+  # # When values$highlight changes, unhighlight the old state (if any) and
+  # # highlight the new state
+  # observe({
+  #   if (length(lastHighlighted) > 0)
+  #     drawStates(getStateName(lastHighlighted), FALSE)
+  #   lastHighlighted <<- values$highlight
+  # 
+  #   if (is.null(values$highlight))
+  #     return()
+  # 
+  #   isolate({
+  #     drawStates(getStateName(values$highlight), TRUE)
+  #   })
+  # })
 })
 
--- ui.R
+++ ui.R
@@ -3,45 +3,32 @@
 
 library(leaflet)
 
-shinyUI(fluidPage(
-  
-  # Add a little CSS to make the map background pure white
-  tags$head(tags$style("
-                       #showcase-code-position-toggle, #showcase-sxs-code { display: none; }
-                       .floater { background-color: white; padding: 8px; opacity: 0.7; border-radius: 6px; box-shadow: 0 0 15px rgba(0,0,0,0.2); }
-                       ")),
-  
-  leafletMap(
-    "map", "100%", 500,
-    # By default OpenStreetMap tiles are used; we want nothing in this case
-    #initialTileLayer = NULL,
-    #initialTileLayerAttribution = NULL,
-    options=list(
-      center = c(40, -98.85),
-      zoom = 4,
-      maxBounds = list(list(17, -180), list(59, 180))
-    )
+
+ui <-(fluidPage(
+
+  sidebarLayout(sidebarPanel(
+    h3("Display Options", align = "center"),
+    sliderInput("month","Select Month", min = 1, max = 12, value = 1),
+    checkboxGroupInput("normvars","Normalization Options", 
+                       c("By Population" = "pop", "By Area" = "area"))
   ),
+                
+    mainPanel(style="background: #ddd;",
+      h3("2016 NCDC Hail Report Data", align = "center"),
+      
+  #my code with newer leaflet stuff
+  leafletOutput("myMap", width = "100%", height = 500)
+  #end my code
+
+  #   absolutePanel(
+  #   right = 520, top = 450, width = 350, class = "floater",
+  #   
+  #   h4("Number Of Reported Severe Hail Events"),
+  #   uiOutput("stateInfo")
+  # )
   
-  absolutePanel(
-    right = 30, top = 10, width = 200, class = "floater",
-    
-    h4("US Population Density"),
-    uiOutput("stateInfo")
-  ),
-  
-  absolutePanel(
-    right = 30, top = 280, style = "", class = "floater",
-    tags$table(
-      mapply(function(from, to, color) {
-        tags$tr(
-          tags$td(tags$div(
-            style = sprintf("width: 16px; height: 16px; background-color: %s;", color)
-          )),
-          tags$td(from, "-", to)
-        )
-      }, densityRanges$from, densityRanges$to, palette, SIMPLIFY=FALSE)
-    )
-  )
+
+  ), 
+ position = "left")
   ))
 
